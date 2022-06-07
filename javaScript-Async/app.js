b8a74087c5b142796044a44e98f36d6dd14d1dae
unknown <RYZEN7@SISTEMAS-WEB.aguainmaculada.local>
2022-06-07 17:53:49 -0500

    funcion Async/Await

--- javaScript-Async/app.js
+++ javaScript-Async/app.js
@@ -45,6 +45,58 @@
 //}
 
 //getUsuarios();
+//*! DE Callbacks a promesas 
+const  getUsuariosIDs = new Promise((response , reject)=>{
+    setTimeout(() => {
+        response([101,102,103,105]);
+    }, 1500);
+})
+
+//const getUsuario = userID =>{
+    //return new Promise((resolve, reject)=>{
+        //setTimeout(id => {
+            //const usuario ={
+                //email:'Allis2897@mail.com',
+                //nombre : 'Allison'
+            //}
+            //resolve(`${id} - ${usuario.email} - ${usuario.nombre}`);
+
+        //},1500,userID);
+    //})
+//}
+
+//const getPermisos = id =>{
+    //return new Promise ((resolve, reject)=>{
+            //setTimeout(id => {
+                //const permiso =['admin','creador'];
+                //resolve(permiso);
+            //}, 1500,id);
+    //});
+//}
+
+//getUsuariosIDs.then(IDs =>{
+    //console.log(IDs);
+    //return getUsuario(IDs[3]);
+//})
+//.then(usuario =>{
+    //console.log(usuario);
+    //return getPermisos(usuario.id);
+//})
+//.then(permisos =>{
+    //console.log(permisos);
+//})
+//.catch(()=>{
+    //console.log('Error')
+//})
+
+//*!Funcion con Async/Await
+
+async function getUsuariosAW() {
+    const IDs = await getUsuariosIDs;
+    console.log(IDs);    
+}
+
+getUsuariosAW()
 
 //*! Promesas
 //* El objeto Promise es usado para computaciones asincronas. Una promesarepresenta un valor que puede estar disponible ahora, en el futuro , o nunca
@@ -52,33 +104,33 @@
 
 //*Ejemplo del uso de promesas
 //*Ejemplo 1:
-let promesa = new Promise((resolve , reject) =>{
-    if(false){
-        resolve('La operación fue exitosa');
-    }else{
-        reject ('Hubo un error');
-    }
-});
+//let promesa = new Promise((resolve , reject) =>{
+    //if(false){
+        //resolve('La operación fue exitosa');
+    //}else{
+        //reject ('Hubo un error');
+    //}
+//});
 
-promesa.then(response =>{
-    console.log('Response:'+ response);
+//promesa.then(response =>{
+    //console.log('Response:'+ response);
 
-}).catch (error =>{
-    console.log('Error:'+ error);
-})
+//}).catch (error =>{
+    //console.log('Error:'+ error);
+//})
 
 //*Ejemplo 2
 
-var miPromesa = Promise.resolve('comida');
-miPromesa.then(resp => console.log(resp));
+//var miPromesa = Promise.resolve('comida');
+//miPromesa.then(resp => console.log(resp));
 
 //*Ejemplo 3
 
-var miPromesa2 = new Promise (function(resolve,reject){
-    setTimeout(() => resolve(4),2000);
-})
+//var miPromesa2 = new Promise (function(resolve,reject){
+    //setTimeout(() => resolve(4),2000);
+//})
 
-miPromesa2.then(resp =>{
-    resp += 5;
-    console.log(resp)
-})
+//miPromesa2.then(resp =>{
+    //resp += 5;
+    //console.log(resp)
+//})
